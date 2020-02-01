9e66ffda221d38828f1f5d22538daaa5a91edf9e
AutoRunCD-Johnny <autoruncd.johnny@gmail.com>
2020-02-01 00:34:23 -0500

    minor bug fixed

--- sector.py
+++ sector.py
@@ -6,7 +6,7 @@ from timeit import default_timer as timer
 
 T=4
 w=32
-h=32
+h=24
 l=100
 n=w/T
 
@@ -36,30 +36,49 @@ class mt_mesh(threading.Thread):
 def mesh(id):
 	for px in range(int(n*id),int(n*(1+id))):
 		for py in range(h):
-			r=R-dr*(py+0.5)
-			theta=np.pi*0.5+amax-da*(px+0.5)
+			r=R-dr*py
+			theta=np.pi*0.5+amax-da*px
 			map[py,px,0]=r*np.cos(theta)
 			map[py,px,1]=r*np.sin(theta)
 
 def st_mesh():
 	for px in range(w):
 		for py in range(h):
-			r=R-dr*(py+0.5)
-			theta=np.pi*0.5+amax-da*(px+0.5)
-			map[py,px,0]=r*np.cos(theta)
-			map[py,px,1]=r*np.sin(theta)
+			r=R-dr*py
+			theta=np.pi*0.5+amax-da*px
+			map[py,px,0]=int(r*np.cos(theta)+w_o)
+			map[py,px,1]=int(h_o+l-r*np.sin(theta))
 
 def gl_mesh():
 	dx,dy=2.0/w,2.0/h
+	u=float(h)/w
 	for i in range(w):
 		for j in range(h):
-			ax,ay=-1+dx*(i+0.5),-1+dy*(j+0.5)
-			px,py=0.5*w*(1+ax),0.5*h*(1+ay)
+			ax,ay=-1+dx*(i+0.5),u-dy*(j+0.5)
+			px,py=0.5*w*(1+ax),0.5*h*(u-ay)
 			r=R-dr*py
 			theta=np.pi*0.5+amax-da*px
 			map[j,i,0]=r*np.cos(theta)/w_o
-			map[j,i,1]=(0.5*h_o+l-r*np.sin(theta))/(0.5*h_o)
-			print("(",map[j,i,0],",",map[j,i,1],",",ax,",",ay,",",px,",",py,")","\t")
+			map[j,i,1]=(r*np.sin(theta)-l-0.5*h_o)/(0.5*h_o)
+			print("(",map[j,i,0],",",map[j,i,1],",",ax,",",ay,",",px,",",py,",",int((map[j,i,0]+1)*w_o),",",int((1-map[j,i,1])*h_o/2.0),")","\t")
+		print("\n")
+
+def gl_mesh2():
+	dx,dy=2.0/w,2.0/h
+	u=float(h)/w
+	l_gl=float(l)*2/w
+	R_gl=np.sqrt((l_gl+2*u)**2+1)
+	Rmin_gl=np.sqrt(l_gl**2+1)
+	dr_gl=(R_gl-Rmin_gl)/float(h)
+	h_o_gl=R_gl-l_gl
+	for i in range(w):
+		for j in range(h):
+			ax,ay=-1+dx*(i+0.5),1-dy*(j+0.5)
+			r=R_gl-dr_gl*(0.5+j)
+			theta=np.pi*0.5+amax-da*(0.5+i)
+			map[j,i,0]=r*np.cos(theta)*w*0.5/w_o
+			map[j,i,1]=r*np.sin(theta)-l_gl-0.5*h_o_gl
+			print("(",map[j,i,0],",",map[j,i,1],",",ax,",",ay,",",int((map[j,i,0]+1)*w_o),",",int((1-map[j,i,1])*h_o/2.0),")","\t")
 		print("\n")
 
 start=timer()
@@ -75,17 +94,18 @@ start=timer()
 #	t.join()
 
 gl_mesh()
+# gl_mesh2()
 st_mesh()
 end=timer()
 
-print("p0=(",int(map[0,0,0]+w_o),",",int(h_o+l-map[0,0,1]),")","p1=(",int(map[h-1,w-1,0]+w_o),",",int(h_o+l-map[h-1,w-1,1]),")")
+print("p0=(",map[0,0,0],",",map[0,0,1],")","p1=(",map[h-1,w-1,0],",",map[h-1,w-1,1],")")
 
 raw=mpimg.imread('2020.01.19-01.26.png')
 
 for px in range(w):
     for py in range(h):
-        x_o=int(map[py,px,0]+w_o)
-        y_o=int(h_o+l-map[py,px,1])
+        x_o=int(map[py,px,0])
+        y_o=int(map[py,px,1])
         out[y_o,x_o,:]=raw[py,px,:]
 
 
